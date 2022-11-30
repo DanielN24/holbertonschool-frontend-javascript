3d7f655c5c178273a16a8b46622d01bc04df48a3
DanielN24 <capdaniel_98@outlook.com>
2022-11-30 09:16:19 -0500

    new files with the priject

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-param-reassign */
+export default function appendToEachArrayValue(array, appendString) {
+  for (const idx of array) {
+    const value = array[idx];
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}
+/* eslint-disable no-param-reassign */
