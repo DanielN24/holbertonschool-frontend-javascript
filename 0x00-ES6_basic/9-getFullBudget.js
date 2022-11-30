b58df8887d8f36193e2c2193064ee5b6123357ce
DanielN24 <capdaniel_98@outlook.com>
2022-11-30 10:31:22 -0500

    correction file 9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,15 +1,14 @@
 /* eslint-disable */
 import getBudgetObject from './7-getBudgetObject.js';
-/* eslint-disable */
-
+ /* eslint-disable */
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars() {
-      return `s${income}`;
+    getIncomeInDollars(income) {
+      return `$${income}`;
     },
-    getIncomeInEuros() {
+    getIncomeInEuros(income) {
       return `${income} euros`;
     },
   };
