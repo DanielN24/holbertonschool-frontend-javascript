77683afc1ec24ec2ef851b6d553e7faea47c55b1
DanielN24 <capdaniel_98@outlook.com>
2022-12-01 14:22:13 -0500

    new file with the answer number 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-unused-vars
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount._name * conversionRate.Currency);
+  }
+}
