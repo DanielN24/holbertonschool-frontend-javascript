a57559ccf24b2a27b77e55fcbf336dc890c6845b
DanielN24 <capdaniel_98@outlook.com>
2022-12-01 17:12:30 -0500

    new file with the answer number 8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,14 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](value) {
+    if (value === 'string') {
+      return this._location;
+    }
+
+    return this._size;
+  }
+}
