610225287c6794564cec6c43bc2508c49996978b
DanielN24 <capdaniel_98@outlook.com>
2022-12-01 17:27:43 -0500

    new file with the answer number 9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -12,6 +12,7 @@ export class HolbertonClass {
     return this._location;
   }
 }
+
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
