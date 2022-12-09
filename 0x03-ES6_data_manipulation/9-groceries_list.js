c2e57be85189afca7bd91486197dfa90089bdb59
DanielN24 <capdaniel_98@outlook.com>
2022-12-08 21:04:09 -0500

    new file with the answer number 8 and 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const list = new Map();
+  list.set('Apples', 10);
+  list.set('Tomatoes', 10);
+  list.set('Pasta', 1);
+  list.set('Rice', 1);
+  list.set('Banana', 5);
+  return list;
+}
