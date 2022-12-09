a2472b3bbb35b4a2ed52fb36d5a9f02c552f8ff5
DanielN24 <capdaniel_98@outlook.com>
2022-12-08 20:59:06 -0500

    new file with the answer number 4 and 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,5 @@
 export default function getStudentIdsSum(list) {
-    return list.reduce(
-      (accumulator, item) => accumulator + item.id, 0,
-    );
-  }
+  return list.reduce(
+    (accumulator, item) => accumulator + item.id, 0,
+  );
+}
--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,9 @@
 export default function updateStudentGradeByCity(list, city, newGrade) {
-    return list
-      .filter((item) => item.location === city)
-      .map((student) => {
-        const theGrade = newGrade.find((grade) => grade.studentId === student.id);
-        const grade = theGrade ? theGrade.grade : 'N/A';
-        return { ...student, grade };
-      });
-  }
+  return list
+    .filter((item) => item.location === city)
+    .map((student) => {
+      const theGrade = newGrade.find((grade) => grade.studentId === student.id);
+      const grade = theGrade ? theGrade.grade : 'N/A';
+      return { ...student, grade };
+    });
+}
